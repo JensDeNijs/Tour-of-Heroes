eaea30a91458acdd86a0a2e56ecf10ecb7c66b40
jens <jensdenijs@hotmail.com>
2021-09-23 11:21:54 +0200

    messages, service/component. history of selected displayed

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,34 +1,33 @@
-import {Component, OnInit} from '@angular/core';
-import {Hero} from '../hero/hero.interface';
-import { HeroService } from "../hero.service";
+import { Component, OnInit } from '@angular/core';
+
+import { Hero } from '../hero/hero.interface';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css']
 })
-
 export class HeroesComponent implements OnInit {
-
-  heroes : Hero[] = [];
 
   selectedHero?: Hero;
 
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
   }
 
-
-  constructor(private heroService: HeroService) {
-  }
-
-  ngOnInit(): void {
-    this.getHeroes()
-  }
   getHeroes(): void {
     this.heroService.getHeroes()
       .subscribe(heroes => this.heroes = heroes);
   }
 }
-
-
